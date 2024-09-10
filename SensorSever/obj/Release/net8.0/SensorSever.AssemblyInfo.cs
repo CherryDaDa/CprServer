fc66d748e50a71c8458dc3e2beee6a4502a32ed8
Sigo_GUGUmori <1262506830@qq.com>
2024-09-10 15:30:25 +0800

    更新了传感器代码，导入port库

--- SensorSever/obj/Release/net8.0/SensorSever.AssemblyInfo.cs
+++ SensorSever/obj/Release/net8.0/SensorSever.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SensorSever")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ceb4b571982bd57263840e38722a5613f77240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+774f8f7b1a901f013d419ee0de8deca51c3c1deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SensorSever")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SensorSever")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
